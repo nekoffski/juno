b1d518cfcebdaff480f40e3f371206a70f90b7c5
Kacper Czyz <kacperczyz@protonmail.com>
2026-04-11 13:54:07 +0200

    fixes

--- cmd/juno-web/main.go
+++ cmd/juno-web/main.go
@@ -78,13 +78,6 @@ func main() {
 	log.Fatal().Err(e.Start(fmt.Sprintf(":%d", webPort))).Msg("web server stopped")
 }
 
-func envOr(key, def string) string {
-	if v := os.Getenv(key); v != "" {
-		return v
-	}
-	return def
-}
-
 func envIntOr(key string, def int) int {
 	if v := os.Getenv(key); v != "" {
 		if n, err := strconv.Atoi(v); err == nil {
